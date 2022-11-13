9edbb606a12172ce7a14b4fe328086b20538bc99
lucas lima <ljesus@reconcavo.org.br>
2022-11-13 18:01:42 -0300

    recovery point

--- lib/dialogBuilder.dart
+++ lib/dialogBuilder.dart
@@ -4,37 +4,6 @@ import 'package:http/http.dart' as http;
 
 import 'dart:convert';
 
-Future<Album> req(url) async {
-  final response = await http.get(Uri.parse('$url'));
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response, then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response, then throw an exception.
-    throw Exception('Falha no carregamento');
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
 // daqui pra cima doc flutter
 
 Future<void> dialogBuilder(BuildContext context) {
@@ -43,7 +12,7 @@ Future<void> dialogBuilder(BuildContext context) {
     builder: (BuildContext context) {
       return AlertDialog(
         title: const Text('Detalhes'),
-        content: Text(const Album(userId: 1, id: 1, title: 'title')),
+        content: const Text('a'),
         actions: <Widget>[
           TextButton(
             style: TextButton.styleFrom(
